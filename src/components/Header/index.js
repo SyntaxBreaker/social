85e8e0ed1154bda48ea1d9f60e9b71d109ee4661
MetaneRuS <xmarius224x@gmail.com>
2021-05-05 14:26:23 +0200

    Update Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,16 @@
 import React, {useContext} from 'react';
-import {signInWithGoogle, signOut} from "../../firebase/firebase";
+import {signInWithGoogle} from "../../firebase/firebase";
 import {UserContext} from "../../providers/UserProvider";
 
 function Header() {
     const user = useContext(UserContext);
 
     return (
-        <header>
-            {user ?
-                <button onClick={() => signOut()}>Log out!</button> :
-                <button onClick={() => signInWithGoogle()}>Log in!</button>
-            }
-        </header>
+        <div>
+            <img src="" alt="logo" />
+            <input type="text" />
+            {!user ? <button onClick={signInWithGoogle}>Log in</button> : <button>Tweet</button>}
+        </div>
     )
 }
 
