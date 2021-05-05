852e15788bab1ae1c1fcb72b3c1d4eae44d27be4
MetaneRuS <xmarius224x@gmail.com>
2021-05-05 22:41:13 +0200

    Add a profile redirect and a button to log out

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {UserContext} from "../../providers/UserProvider";
+import {signOut} from "../../firebase/firebase";
 import {Link} from "react-router-dom";
 import './index.scss';
 
 function Navigation() {
+    const user = useContext(UserContext);
+
     return (
         <div className="navigation">
             <ul>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
-                    <Link to="profile">Profile</Link>
-                </li>
+                {user &&
+                    <>
+                        <li>
+                            <Link to={`/profile/${user.uid}`}>Profile</Link>
+                        </li>
+                        <li>
+                            <button onClick={signOut}>Log out</button>
+                        </li>
+                    </>
+                }
             </ul>
         </div>
     )
