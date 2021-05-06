0ef811a8de8a2938feba5ffcedb553345d4ee995
MetaneRuS <xmarius224x@gmail.com>
2021-05-06 13:55:49 +0200

    Add icon to Header and update styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,14 @@ import React, {useContext} from 'react';
 import {signInWithGoogle} from "../../firebase/firebase";
 import {UserContext} from "../../providers/UserProvider";
 import './index.scss';
+import * as Icon from 'react-feather';
 
 function Header() {
     const user = useContext(UserContext);
 
     return (
         <header>
-            <a href="/"><img src="" alt="logo" /></a>
+            <a href="/"><Icon.Twitter /></a>
             <input type="text" placeholder="Search on ..." />
             {!user ? <button onClick={signInWithGoogle}>Log in</button> : <button>Tweet</button>}
         </header>
