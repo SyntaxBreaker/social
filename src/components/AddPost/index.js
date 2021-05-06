153abd9f3ac3fdc362f25dda021fd09d1eede86f
MetaneRuS <xmarius224x@gmail.com>
2021-05-06 18:09:39 +0200

    Add showModal if user is not logged

--- src/components/AddPost/index.js
+++ src/components/AddPost/index.js
@@ -3,11 +3,13 @@ import {UserContext} from "../../providers/UserProvider";
 import firebase from "firebase";
 import {db} from "../../firebase/firebase";
 import './index.scss';
+import Modal from "../Modal";
 
 function AddPost() {
     const user = useContext(UserContext);
     const [newPostContent, setNewPostContent] = useState('');
     const [newPostTags, setNewPostTags] = useState([]);
+    const [showModal, setShowModal] = useState(false);
 
     const onChange = event => {
         const tags = event.target.value.split(' ').filter(item => item.includes('#'));
@@ -35,10 +37,25 @@ function AddPost() {
         setNewPostContent('');
         setNewPostTags('');
     }
+
+    const toggleModal = (event) => {
+        event.preventDefault();
+        setShowModal(true);
+    }
+
+    const handleClose = () => {
+        setShowModal(false);
+    }
+
     return (
-        <form onSubmit={event => onSubmit(event)}>
-            <input type="text" name="newPost" value={newPostContent} placeholder={`What's happening?`} onChange={(event) => onChange((event))} />
-        </form>
+       <>
+           {user ? <form onSubmit={event => onSubmit(event)}>
+               <input type="text" name="newPost" value={newPostContent} placeholder={`What's happening?`} onChange={(event) => onChange((event))} />
+           </form> : <form onSubmit={(event) => toggleModal(event)}>
+               <input type="text" name="newPost" value={newPostContent} placeholder={`What's happening?`} onChange={(event) => onChange((event))} />
+           </form>}
+           {(!user && showModal) && <Modal handleClose={handleClose} />}
+       </>
     )
 }
 
