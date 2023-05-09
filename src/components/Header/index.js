e5eaffc7ef022eced49ae50067150ccbd1eb5bbe
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-05-09 13:18:48 +0200

    Improve accessibility

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ function Header() {
 
     return (
         <header className="header">
-            <a href="/" className="header__logo"><img src={logo} className="header__img" /> Social</a>
+            <a href="/" className="header__logo"><img src={logo} className="header__img" alt='Website logo' /> Social</a>
             {!user ? <button className="header__button" onClick={signInWithGoogle}>Log in</button> : <button className="header__button" onClick={signOut}>Log out</button>}
         </header>
     )
