921cc5c449274de926799ec13b627f5092516ac1
xMetaneRx <60369185+xMetaneRx@users.noreply.github.com>
2023-05-10 13:06:37 +0200

    Add mobile navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,45 @@
-import React, {useContext} from 'react';
-import {signInWithGoogle, signOut} from "../../firebase/firebase";
-import {UserContext} from "../../providers/UserProvider";
+import React, { useContext, useState, useEffect } from 'react';
+import { signInWithGoogle, signOut } from "../../firebase/firebase";
+import { UserContext } from "../../providers/UserProvider";
 import logo from '../../logo.svg';
 import './index.scss';
+import Navigation from '../Navigation/index';
 
 function Header() {
+    const [isMobile, setIsMobile] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const user = useContext(UserContext);
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth <= 900) {
+                setIsMobile(true);
+            } else {
+                setIsMobile(false);
+            }
+        }
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (isMobile && isOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "initial";
+        }
+    }, [isMobile, isOpen]);
 
     return (
         <header className="header">
             <a href="/" className="header__logo"><img src={logo} className="header__img" alt='Website logo' /> Social</a>
-            {!user ? <button className="header__button" onClick={signInWithGoogle}>Log in</button> : <button className="header__button" onClick={signOut}>Log out</button>}
+            {isMobile ? <button onClick={() => setIsOpen(!isOpen)} className="header__button--menu">☰</button> : !user ? <button className="header__button" onClick={signInWithGoogle}>Log in</button> : <button className="header__button" onClick={signOut}>Log out</button>}
+            {(isOpen && isMobile) && <Navigation isMobile={isMobile} setIsOpen={setIsOpen} />}
         </header>
     )
 }
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import { UserContext } from "../../providers/UserProvider";
-import { signOut } from "../../firebase/firebase";
+import { signInWithGoogle, signOut } from "../../firebase/firebase";
 import { Link } from "react-router-dom";
 import './index.scss';
 import * as Icon from 'react-feather';
 
-function Navigation() {
+function Navigation({ isMobile, setIsOpen }) {
     const user = useContext(UserContext);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+        function handleOutsideClick(event) {
+            if (isMobile && menuRef.current && !menuRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        }
+    }, [])
 
     return (
         <div className="navigation">
-            <ul className="navigation__list">
-                <li className="navigation__item">
-                    <Link to="/" className="navigation__link"><Icon.Home /> Home</Link>
-                </li>
-                <li className="navigation__item">
-                    <Link to={`/profile/${user.uid}`} className="navigation__link"><Icon.User /> Profile</Link>
-                </li>
-                <li className="navigation__item">
-                    <Link to={`/settings`} className="navigation__link"><Icon.Settings /> Settings</Link>
-                </li>
-                <li className="navigation__item">
-                    <button onClick={signOut} className="navigation__btn"><Icon.LogOut /> Log out</button>
-                </li>
+            <ul className="navigation__list" ref={menuRef} onClick={() => isMobile && setIsOpen(false)}>
+                {user ?
+                    <>
+                        <li className="navigation__item">
+                            <Link to="/" className="navigation__link"><Icon.Home /> Home</Link>
+                        </li>
+                        <li className="navigation__item">
+                            <Link to={`/profile/${user.uid}`} className="navigation__link"><Icon.User /> Profile</Link>
+                        </li>
+                        <li className="navigation__item">
+                            <Link to={`/settings`} className="navigation__link"><Icon.Settings /> Settings</Link>
+                        </li>
+                        <li className="navigation__item">
+                            <button onClick={signOut} className="navigation__btn"><Icon.LogOut /> Log out</button>
+                        </li>
+                    </>
+                    : <li className="navigation__item">
+                        <button onClick={signInWithGoogle} className="navigation__btn"><Icon.LogIn /> Log in</button>
+                    </li>}
             </ul>
         </div>
     )
